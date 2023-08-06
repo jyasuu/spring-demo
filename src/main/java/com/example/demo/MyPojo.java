3adb2428f747c2722d6877655375b7ac2acbefcf
Leo.yen <leo.yen@gmail.com>
2023-08-06 17:20:00 +0800

    try config

--- src/main/java/com/example/demo/MyPojo.java
+++ src/main/java/com/example/demo/MyPojo.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @ToString
 @Getter
 @Setter
 public class MyPojo {
+    @NotNull
     private String name;
+    @NotNull
     private String description;
 
 }
--- src/main/java/com/example/demo/MyProperties.java
+++ src/main/java/com/example/demo/MyProperties.java
@@ -5,15 +5,18 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import jakarta.validation.Valid;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties("my")
+@Validated
 public class MyProperties {
-
+    @Valid
     private final Map<String, MyPojo> map = new LinkedHashMap<>();
-
+    @Valid
     private final List<MyPojo> list = new ArrayList<>();
 
     public List<MyPojo> getList() {
