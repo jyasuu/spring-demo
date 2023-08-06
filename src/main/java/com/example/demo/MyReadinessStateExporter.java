130bf644fa49c66787ef647838d159311679a299
Leo.yen <leo.yen@gmail.com>
2023-08-06 15:58:59 +0800

    EventListener

---
+++ src/main/java/com/example/demo/MyReadinessStateExporter.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyReadinessStateExporter {
+
+    private final static Logger logger = LoggerFactory.getLogger(MyReadinessStateExporter.class);
+
+    @EventListener
+    public void onStateChange(AvailabilityChangeEvent<ReadinessState> event) {
+        ReadinessState newState = event.getState();
+
+        switch (event.getState()) {
+            case ACCEPTING_TRAFFIC -> {
+                logger.info("Readiness state changed to: " + newState);
+            }
+            // create file /tmp/healthy
+            case REFUSING_TRAFFIC -> {
+                logger.warn("Readiness state changed to: " + newState);
+            }
+            // remove file /tmp/healthy
+        }
+    }
+}
